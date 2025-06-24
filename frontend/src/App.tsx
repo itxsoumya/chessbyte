09280117f17d1dc81858e526c9053525f2b962dc
itxsoumya <itxsoumyaranjan@gmail.com>
2025-06-24 11:17:58 +0530

    shadcn ui added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import Board from "./components/Board"
 import NavBar from "./components/NavBar"
+import { Button } from "./components/ui/button"
 
 
 const App = ()=>{
   return (
-    <div>
+    <div className="p-5">
       <NavBar/>
       <br />
       {/* <hr /> */}
       <Board/>
+<br />
+      <Button className=" w-full p-7 text-xl">Click to Play</Button>
     </div>
   )
 }
