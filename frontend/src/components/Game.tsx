b04ab65fbf7bdb89f20dcc62bf62aca8ab5a62cc
itxsoumya <itxsoumyaranjan@gmail.com>
2025-06-25 20:58:23 +0530

    Game.tsx

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,15 +1,14 @@
 import Board from "./Board";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
 
-const Game = ()=>{
-
-
-  return(
-    <div className="bg-red-100x border-2x max-w-7xl mx-auto p-2 grid md:grid-cols-3">
-
-      <div className="col-span-2">
-      <div className="flex justify-between border">
+const Game = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="bg-red-100x border-2x max-w-6xl mx-auto p-2 grid md:grid-cols-3 grid-cols-1">
+      <div className="md:col-span-2">
+        <div className="flex justify-between border">
           <div className="p-1 flex gap-2 items-center">
             <Avatar className="rounded-sm">
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -22,8 +21,8 @@ const Game = ()=>{
             <Button>00:05</Button>
           </div>
         </div>
-      <Board/>
-      <div className="flex justify-between border">
+        <Board />
+        <div className="flex justify-between border">
           <div className="p-1 flex gap-2 items-center">
             <Avatar className="rounded-sm">
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -38,22 +37,19 @@ const Game = ()=>{
         </div>
       </div>
 
-      <div className="bg-green-300x flex flex-col gap-5 justify-center items-center">
-        <Button className="w-full">Play as a Guest</Button>
-       <div className="flex gap-2">
-        <Button>Login</Button>
-        <Button>Signin</Button>
+      <div className="flex flex-col justify-center items-center p-2">
+        
+       <div className="bg-red-50x w-full grid grid-cols-2 gap-2">
+       <Button variant={"outline"} className="col-span-2 h-16">Play as a Guest</Button>
+        
+        
+        <Button onClick={()=>navigate("/login")} className="">Login</Button>
+        <Button onClick={()=>navigate("/signup")} className="">Signin</Button>
        </div>
+        
       </div>
-
-
-
-
-      
     </div>
-  )
-}
-
-
+  );
+};
 
 export default Game;
