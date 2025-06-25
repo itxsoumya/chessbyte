ec96085a7e5c518bcd23b93428d6510ba333606b
itxsoumya <itxsoumyaranjan@gmail.com>
2025-06-25 20:16:22 +0530

    grid used

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -2,11 +2,14 @@ import Board from "./Board";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
 
-const Game = () => {
-  return (
-    <div className="bg-red-100x border-2x max-w-7xl mx-auto flex gap-5 max-sm:flex-col p-2">
-      <div>
-        <div className="flex justify-between border">
+const Game = ()=>{
+
+
+  return(
+    <div className="bg-red-100x border-2x max-w-7xl mx-auto p-2 grid md:grid-cols-3">
+
+      <div className="col-span-2">
+      <div className="flex justify-between border">
           <div className="p-1 flex gap-2 items-center">
             <Avatar className="rounded-sm">
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -19,10 +22,8 @@ const Game = () => {
             <Button>00:05</Button>
           </div>
         </div>
-        {/* board */}
-        <Board />
-
-        <div className="flex justify-between border">
+      <Board/>
+      <div className="flex justify-between border">
           <div className="p-1 flex gap-2 items-center">
             <Avatar className="rounded-sm">
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -36,9 +37,23 @@ const Game = () => {
           </div>
         </div>
       </div>
-      <div className="bg-sky-100">Moves</div>
+
+      <div className="bg-green-300x flex flex-col gap-5 justify-center items-center">
+        <Button className="w-full">Play as a Guest</Button>
+       <div className="flex gap-2">
+        <Button>Login</Button>
+        <Button>Signin</Button>
+       </div>
+      </div>
+
+
+
+
+      
     </div>
-  );
-};
+  )
+}
+
+
 
 export default Game;
